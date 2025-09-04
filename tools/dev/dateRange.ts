feb21002ea03e0687992ab28a15b899ef9252157
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-09-04 13:21:39 +0800

    tools

---
+++ tools/dev/dateRange.ts
@@ -0,0 +1,30 @@
+import { db } from "@server/db";
+import { DatabaseAssetService } from "@server/services/assets/database";
+import { QueryParamsFilterToDateRange } from "@shared/utils/assets";
+
+const assetService = new DatabaseAssetService(db);
+
+const query = {
+  filter: {
+    start: { eq: "2024-01-01" },
+    end: { eq: "2025-09-04" },
+  },
+};
+
+const run = async () => {
+  const result = await assetService.getPortfolioValueHistoryForUser(
+    "f4236b2a-7114-4c21-b8c2-531889dee544"
+    //`query
+  );
+
+  return result;
+};
+
+run()
+  .then((dateRange) => {
+    console.log("done");
+    //console.log(dateRange);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
