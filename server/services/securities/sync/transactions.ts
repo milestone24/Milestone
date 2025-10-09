8cebdf601f9ddb4ce6b29829e33d9d79ef949a18
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-10-09 13:40:15 +0800

    stub for validating security transactions

---
+++ server/services/securities/sync/transactions.ts
@@ -0,0 +1,9 @@
+export const validateSecurityTransaction = (
+  assetId: string,
+  securityId: string,
+  transactionId: string
+) => {
+  /**
+   * Security transactions should have a way of being validated against live stock prices.
+   */
+};
