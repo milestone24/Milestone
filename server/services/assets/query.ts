4e90819bd267c2da2e7ad1a16890eb05b7b3476d
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-10-31 12:48:22 +0800

    should be reusable db query for calculated asset with recurring contributions

--- server/services/assets/query.ts
+++ server/services/assets/query.ts
@@ -1,9 +1,17 @@
-import { assetValues } from "@server/db/schema";
+import { assetValues, recurringContributions } from "@server/db/schema";
 import { Database } from "@server/db";
 import { userAssets } from "@server/db/schema";
 import { eq, getTableColumns, sql } from "drizzle-orm";
 
-export const assetsQueryBuilder = (db: Database) =>
+const lateralLatestValueSql = sql`LATERAL (
+                  SELECT ${assetValues.value}, ${assetValues.valueDate}
+                  FROM ${assetValues}
+                  WHERE ${assetValues.assetId} = ${userAssets.id}
+                  ORDER BY ${assetValues.valueDate} DESC
+                  LIMIT 1
+                ) AS latest_value`;
+
+export const calculatedAssetsQueryBuilder = (db: Database) =>
   db
     .select({
       ...getTableColumns(userAssets),
@@ -11,13 +19,21 @@ export const assetsQueryBuilder = (db: Database) =>
       lastValueDate: sql<Date | null>`latest_value.value_date`,
     })
     .from(userAssets)
+    .leftJoin(lateralLatestValueSql, sql`true`);
+
+export const calculatedAssetsWithContributionsQueryBuilder = (db: Database) =>
+  db
+    .select({
+      asset: {
+        ...getTableColumns(userAssets),
+        currentValue: sql<number>`COALESCE(latest_value.value, 0)`,
+        lastValueDate: sql<Date | null>`latest_value.value_date`,
+      },
+      recurringContribution: recurringContributions,
+    })
+    .from(userAssets)
+    .leftJoin(lateralLatestValueSql, sql`true`)
     .leftJoin(
-      sql`LATERAL (
-                  SELECT ${assetValues.value}, ${assetValues.valueDate}
-                  FROM ${assetValues}
-                  WHERE ${assetValues.assetId} = ${userAssets.id}
-                  ORDER BY ${assetValues.valueDate} DESC
-                  LIMIT 1
-                ) AS latest_value`,
-      sql`true`
+      recurringContributions,
+      sql`${userAssets.id} = ${recurringContributions.assetId}`
     );
