5356406920f64b12d790db81996915453e0d3d13
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-11-11 16:36:44 +0800

    use tailwind v4 and vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { VitePWA } from "vite-plugin-pwa";
-
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     VitePWA({
       registerType: "autoUpdate",
