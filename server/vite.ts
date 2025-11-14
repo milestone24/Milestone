e780370bdd922dfccf699c0726bbcefb31827d3b
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-11-14 13:14:46 +0800

    vite module should use express 5 route syntax for catch all

--- server/vite.ts
+++ server/vite.ts
@@ -113,7 +113,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.use("/*all", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
