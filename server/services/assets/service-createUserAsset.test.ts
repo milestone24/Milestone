e9328b21f8ca8e34710c6a604778b321f554c565
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-11-20 12:35:44 +0800

    create user asset should have uit tests (wip)

---
+++ server/services/assets/service-createUserAsset.test.ts
@@ -0,0 +1,112 @@
+import { db } from "@server/db";
+import { DatabaseAssetService } from "./database";
+import { DatabaseUserService } from "@server/services/users/database";
+import { beforeAll, describe, it, expect, vi } from "vitest";
+import { createDecimalValueString } from "@shared/schema/utils";
+import { BrokerPlatform, UserAssetInsert } from "@shared/schema";
+import { createRRulePattern } from "@shared/utils/scheduling";
+import { subDays } from "date-fns";
+
+describe("DatabaseAssetService createUserAsset", () => {
+  let databaseAssetService: DatabaseAssetService;
+  let databaseUserService: DatabaseUserService;
+  let platform: BrokerPlatform;
+  let userAccountId: string;
+
+  beforeAll(async () => {
+    databaseAssetService = new DatabaseAssetService(db);
+    databaseUserService = new DatabaseUserService(db);
+
+    const platforms = await databaseAssetService.getBrokerPlatforms();
+    if (!platforms || platforms.length === 0) {
+      throw new Error("No broker platforms found");
+    }
+    platform = platforms[0]!;
+
+    const userAccount = await databaseUserService.getUserAccountByEmail(
+      "gary@milestone.com"
+      //"chris@milestone.com"
+    );
+
+    if (!userAccount) {
+      throw new Error("User account not found");
+    }
+    userAccountId = userAccount.id;
+  });
+
+  it(
+    "should create a user asset",
+    async () => {
+      const updateAssetValuesSpy = await vi.spyOn(
+        databaseAssetService,
+        "updateAssetValues"
+      );
+      // .mockImplementation(async () => {
+      //   return Promise.resolve();
+      // });
+
+      const startDate = subDays(new Date(), 2);
+
+      const data: UserAssetInsert = {
+        name: "Test Asset",
+        accountType: "ISA",
+        platformId: platform.id,
+        currentValue: createDecimalValueString("0"),
+        valueMethod: "calculated",
+        startDate,
+        userAccountId,
+        securities: [
+          {
+            tempId: "5d627060-d23a-448a-ad8d-e93292c0f3c5",
+            security: {
+              symbol: "APPS",
+              name: "Digital Turbine Inc",
+              sourceIdentifier: "eodhd",
+              exchange: "US",
+              country: "USA",
+              currency: "USD",
+              type: "Common Stock",
+              isin: "US25400W1027",
+            },
+            shareHolding: createDecimalValueString("10000"),
+            currencyValue: createDecimalValueString("100"),
+            startDate,
+            priorGainLoss: createDecimalValueString("0"),
+          },
+        ],
+        contributions: {
+          amount: createDecimalValueString("200"),
+          process: "manual",
+          startDate,
+          patternConfig: {
+            type: "rrule",
+            expression: createRRulePattern(
+              "FREQ=MONTHLY;INTERVAL=1;BYMONTHDAY=1"
+            ).expression,
+          },
+          notificationEmail: false,
+          notificationPush: false,
+          isActive: true,
+          type: "security",
+          securityDistribution: [
+            {
+              securityId: "5d627060-d23a-448a-ad8d-e93292c0f3c5",
+              isTempSecurityId: true,
+              securityName: "Digital Turbine Inc",
+              commitment: createDecimalValueString("100"),
+            },
+          ],
+        },
+      };
+
+      const asset = await databaseAssetService.createUserAsset(data);
+
+      expect(asset).toBeDefined();
+      expect(updateAssetValuesSpy).toHaveBeenCalledWith(
+        data.userAccountId,
+        asset.id
+      );
+    },
+    60000 * 10
+  );
+});
