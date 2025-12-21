d4016be2c976b1719fb3c47912d26759c672350a
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-12-21 09:40:35 +0800

    socket notifiations should be own module

---
+++ server/services/comms/socket.ts
@@ -0,0 +1,11 @@
+import { connections } from "@server/sockets/connections";
+import { SocketMessage } from "@shared/schema/socket";
+
+export const sendNotification = (accountId: string, message: SocketMessage) => {
+  const sockets = connections.get(accountId);
+  if (sockets) {
+    sockets.forEach((socket) => {
+      socket.send(JSON.stringify(message));
+    });
+  }
+};
