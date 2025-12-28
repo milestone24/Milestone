679208f3b69d8f35d933392a2c25b86ea682c1e6
Gary Law <digozi@Garys-MacBook-Pro.local>
2025-12-28 11:00:55 +0800

    trigger routes should use requirScope

--- server/routes/triggers.ts
+++ server/routes/triggers.ts
@@ -1,6 +1,5 @@
 import { AuthService } from "@server/auth";
 import { db } from "@server/db";
-import { assetPersistenceFactory } from "@server/services/assets/database";
 import { AssetValuesService } from "@server/services/process/asset-values";
 import { SecuritiesCacheService } from "@server/services/process/securities-cache";
 import { Router } from "express";
@@ -9,7 +8,7 @@ export async function registerRoutes(
   router: Router,
   authService: AuthService
 ): Promise<Router> {
-  const { requireApiKey } = authService.getAuthMiddlewares();
+  const { requireApiKey, requireScope } = authService.getAuthMiddlewares();
 
   const assetValuesService = new AssetValuesService(db);
   const securitiesCacheService = new SecuritiesCacheService(db);
@@ -17,8 +16,8 @@ export async function registerRoutes(
   router.post(
     "/triggers/asset-values-update",
     requireApiKey,
+    requireScope("trigger"),
     async (req, res) => {
-      const { accountId, assetId } = req.body;
       assetValuesService.updateAssetValuesForAllAssetsOfAllAccounts();
       res.json({ message: "Asset values update has been triggered" });
     }
@@ -27,6 +26,7 @@ export async function registerRoutes(
   router.post(
     "/triggers/securities-daily-history-cache-update",
     requireApiKey,
+    requireScope("trigger"),
     async (req, res) => {
       securitiesCacheService.updateSecuritiesDailyHistoryCacheForAllSecurities();
       res.json({
