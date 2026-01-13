d7352d8685b578ba5ce603e5474a1aa13c05e170
Gary Law <digozi@Garys-MacBook-Pro.local>
2026-01-13 13:57:59 +0800

    there should be a single useAssets hook

---
+++ client/src/hooks/use-assets.ts
@@ -0,0 +1,35 @@
+import { useSession } from "@/hooks/use-session";
+import { getDateUrlParams } from "@/lib/date";
+import { apiRequest } from "@/lib/queryClient";
+import { portfolioAssets } from "@shared/api/queryKeys";
+import {
+  UserAssetWithHistoryAndAccountChange,
+  userAssetWithHistoryAndAccountChangeSchema,
+} from "@shared/schema";
+import { skipToken, useQuery } from "@tanstack/react-query";
+
+export const useAssets = (startDate?: Date, endDate?: Date) => {
+  const { user, isSessionPending } = useSession();
+  const apiEnabled = !isSessionPending && !!user;
+
+  return useQuery<UserAssetWithHistoryAndAccountChange[]>({
+    queryKey: [...portfolioAssets, startDate, endDate],
+    queryFn: apiEnabled
+      ? async () => {
+          const response = await apiRequest<
+            UserAssetWithHistoryAndAccountChange[]
+          >("GET", `/api/assets?${getDateUrlParams(startDate, endDate)}`);
+          const result = userAssetWithHistoryAndAccountChangeSchema
+            .array()
+            .safeParse(response);
+
+          if (!result.success) {
+            console.log("useAssets result", result.error);
+            throw new Error("Invalid assets result");
+          }
+
+          return result.data;
+        }
+      : skipToken,
+  });
+};
