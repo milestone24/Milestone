92bf577dad1d61765fe2af3d455b48751bffc01d
Gary Law <skin@skinv1.co.uk>
2026-02-06 13:42:54 +0800

    diable typecheck for fire-third page

--- client/src/pages/fire-third.tsx
+++ client/src/pages/fire-third.tsx
@@ -1,5 +1,4 @@
 //@ts-nocheck
-
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
