392ae0bf1ecfa2efa8669194f955bb83f7eb19be
Gary Law <skin@skinv1.co.uk>
2026-05-26 11:14:17 +0800

    client: there should be reusable currency format functions

---
+++ client/src/utils/decimal.ts
@@ -0,0 +1,20 @@
+import { DecimalValueString } from "@shared/schema";
+import Decimal from "decimal.js";
+
+
+export const formatCurrencyDecimal = (value: DecimalValueString) => {
+  return Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  }).format(Decimal(value).toNumber());
+};
+
+
+export const formatShareQuantityDecimal = (value: DecimalValueString) => {
+  return Intl.NumberFormat("en-GB", {
+    maximumFractionDigits: 8,
+    minimumFractionDigits: 8,
+  }).format(Decimal(value).toNumber());
+};
